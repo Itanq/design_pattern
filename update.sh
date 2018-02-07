8780e61d0e47b1d90097b086953b8fc17f53159b
zmant <zmant724@topplus.com>
2018-02-07 22:18:31 +0800

    update update

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 
 if [ $# -eq 1 ];then
-    git add *
+    git add . *
     git commit -m "$1"
     git push gitee master
     git push github master
+else
+    echo "写明 git commit 的信息：　./update.sh \"xxxxx\""
 fi
